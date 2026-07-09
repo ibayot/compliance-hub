44ff173eda525de94935f1ac30c216b9fa3c351e
ibayot <ibaymarcjayson@gmail.com>
2026-07-09 14:34:59 +0800

    chore: clean up repository files per user instructions

--- scratch/old-db-init/06-smtp-fallback.sql
+++
@@ -1,6 +0,0 @@
-USE 02_db_stg_compliance_hub_ticketing;
-
-ALTER TABLE ticketing_configs
-ADD COLUMN IF NOT EXISTS primary_smtp_sent_today INT NOT NULL DEFAULT 0,
-ADD COLUMN IF NOT EXISTS primary_smtp_last_sent_date DATE NULL,
-ADD COLUMN IF NOT EXISTS primary_smtp_daily_limit INT NOT NULL DEFAULT 500;
--- scratch/update_pass.sql
+++
@@ -1,1 +0,0 @@
-UPDATE users SET passwordHash = '$2b$10$52MSFQ4I7wuBq2n0ZLoypu5G9oa2hzSXticXLbFJHLSy73cjnwsqa' WHERE email='mpmabazza@dswd.gov.ph';
